4715382f4aea41d8b50afc645db9068077fb4bf3
Shruti  Gawade <99631649+shrutig1602@users.noreply.github.com>
2023-10-20 19:15:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 // Serve static files from the public directory (where your index.html is located)
 app.use(express.static(path.join(__dirname, 'public')));
